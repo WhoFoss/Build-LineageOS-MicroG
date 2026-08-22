770c937b7845e75f6c2fbc1356bcfcaea35229c2
WhoFoss <whofoss@tuta.io>
2026-08-22 06:49:46 -0300

    Update aurorasetup.sh

--- AuroraStore/aurorasetup.sh
+++ AuroraStore/aurorasetup.sh
@@ -23,17 +23,9 @@ get-aurora-components() {
     # atualizar o numero da versao manualmente quando sair release nova
     local aurora_store_url="https://f-droid.org/repo/com.aurora.store_76.apk"
 
-    # AuroraServices nao publica releases formais no GitLab (sem assets na API);
-    # este eh o unico link disponivel, fixo na v1.1.1 (ultimo lancamento, 2021)
-    local aurora_services_url="https://gitlab.com/-/project/8363046/uploads/c22e95975571e9db143567690777a56e/AuroraServices_v1.1.1.apk"
-
     name="AuroraStore"
     mkdir -p "$name"
     download_apk "$aurora_store_url" "$name"
-
-    name="AuroraServices"
-    mkdir -p "$name"
-    download_apk "$aurora_services_url" "$name"
 }
 
 cd vendor/aurora
